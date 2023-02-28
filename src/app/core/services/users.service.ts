e7bfbe34650a8484648ec30f5019eed400863b33
AD <Veselblu@yandex.ru>
2023-03-01 00:10:52 +0300

    Resolve comments backend connect

---
+++ src/app/core/services/users.service.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {ApiService} from "./api.service";
+import {Observable} from "rxjs";
+import {HttpParams} from "@angular/common/http";
+import {ApiError} from "../types/api-error";
+import {Article} from "../../../data/models/article";
+import {ApiResult} from "../types/api-result";
+import User = Article.User;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  private readonly base: string = "/users/";
+
+  constructor(private apiService: ApiService) {
+  }
+
+  getMe(onError: ApiError): Observable<User> {
+    return this.apiService.get(`${this.base}me`, new HttpParams(), onError);
+  }
+}
--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, ElementRef, Input, ViewChild, ViewEncapsulation} from '@angular/core';
 import {Article} from "../../../../data/models/article";
 import {Destination, NavigationService} from "../../../core/services/navigation.service";
 import {environment} from "../../../../environments/environment";
+import {ArticlesService} from "../../../core/services/articles.service";
 
 @Component({
   selector: 'poly-card',
@@ -9,7 +10,10 @@ import {environment} from "../../../../environments/environment";
   styleUrls: ['./card.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
+
+  @ViewChild("root")
+  root!: ElementRef;
 
   @Input()
   article!: Article.Item;
@@ -20,10 +24,14 @@ export class CardComponent implements OnInit {
   @Input()
   isLast: boolean = false;
 
-  constructor(private navigationService: NavigationService) {
+  constructor(private navigationService: NavigationService, private articleService: ArticlesService) {
   }
 
-  ngOnInit(): void {
+  likeDislike(): void {
+    this.articleService.like(this.article.id, () => {
+      this.articleService.disLike(this.article.id, () => {
+      }).subscribe(() => this.article.likes--);
+    }).subscribe(() => this.article.likes++);
   }
 
   toArticle(e: Event): void {
