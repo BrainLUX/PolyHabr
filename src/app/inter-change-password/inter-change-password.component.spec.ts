a2a1a4e12123285ae02479f5a9964eeeedab2c9c
AD <Veselblu@yandex.ru>
2023-03-16 00:18:12 +0300

    Add inter test

--- src/app/inter-change-password/inter-change-password.component.spec.ts
+++ src/app/inter-change-password/inter-change-password.component.spec.ts
@@ -1,21 +1,31 @@
-import {ComponentFixture, TestBed} from "@angular/core/testing";
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {ComponentFixture, fakeAsync, inject, TestBed} from "@angular/core/testing";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import {InterChangePasswordComponent} from "./inter-change-password.component";
+import {DisciplineTypesService} from "../core/services/discipline_types.service";
+import {AuthorizationService} from "../core/services/authorization.service";
+import {Router} from "@angular/router";
+import {Destination} from "../core/services/navigation.service";
 
-describe("Inter change password tests",() => {
+describe("Inter change password tests", () => {
   let component: InterChangePasswordComponent;
   let fixture: ComponentFixture<InterChangePasswordComponent>;
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
+    TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       declarations: [InterChangePasswordComponent]
-    }).compileComponents();
-  });
-  beforeEach(() => {
+    });
     fixture = TestBed.createComponent(InterChangePasswordComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.componentInstance; // SortBarComponent test instance
   });
+  beforeEach(fakeAsync(
+    inject([DisciplineTypesService, HttpTestingController, Router],
+      (authorizationService: AuthorizationService, backend: HttpTestingController, router: Router) => {
+        spyOn(router, 'navigateByUrl').and.stub();
+        fixture.detectChanges();
+        backend.expectOne({}).flush({});
+        expect(router.navigateByUrl).toHaveBeenCalled();
+        expect(router.navigateByUrl).toHaveBeenCalledWith(Destination.CHANGE_PASSWORD.toPath());
+      })));
 
   it('Inter change password component test', () => {
     expect(component).toBeTruthy();
