131694da97e4e70da0763033e428dd2e920b5b05
AD <Veselblu@yandex.ru>
2023-03-27 18:32:20 +0300

    Fix tests

--- cypress/e2e/fav-article.cy.ts
+++ cypress/e2e/fav-article.cy.ts
@@ -7,12 +7,15 @@ it('Fav article', () => {
   cy.url().should('not.include', 'login');
   cy.url().should('include', '/');
   let titleBefore = "";
-  cy.get('poly-card:nth-child(2) article .title').then(($span) => {
-    titleBefore = $span.text();
+  cy.get('poly-card:nth-child(2) article .title').then(($text) => {
+    titleBefore = $text.get()[0].innerText;
   });
   cy.get('poly-card:nth-child(2) article .favourite').click();
+  cy.get('poly-card:nth-child(2) article .favourite svg').should("have.class", "selected");
   cy.get('poly-card:nth-child(2) article > a').click();
-  cy.get('h2.title').should('contain.text', titleBefore);
+  cy.get('h2.title').should((title) => {
+    expect(title.get()[0].innerText, titleBefore);
+  });
   cy.get('article:first-child .favourite svg').should("have.class", "selected");
   cy.get('article:first-child .favourite').click();
 });
--- cypress/e2e/like-article.cy.ts
+++ cypress/e2e/like-article.cy.ts
@@ -8,16 +8,24 @@ it('Like article', () => {
   cy.url().should('include', '/');
   let titleBefore = "";
   cy.get('poly-card:nth-child(2) article .title').then(($span) => {
-    titleBefore = $span.text();
+    titleBefore = $span.get()[0].innerText;
   });
   let likesBefore = 0;
   cy.get('poly-card:nth-child(2) article .rating span').then(($span) => {
-    likesBefore = Number($span.text());
+    likesBefore = Number($span.get()[0].innerText);
   });
   cy.get('poly-card:nth-child(2) article .rating').click();
-  cy.get('poly-card:nth-child(2) .rating span').should('have.text', (likesBefore + 1).toString());
+  let likesAfter = 0;
+  cy.get('poly-card:nth-child(2) .rating span').should(($rating) => {
+    expect($rating.get()[0].innerText, `${likesBefore + 1}`);
+    likesAfter = Number($rating.get()[0].innerText);
+  });
   cy.get('poly-card:nth-child(2) article > a').click();
-  cy.get('h2.title').should('contain.text', titleBefore);
-  cy.get('article:first-child .rating span').should('have.text', (likesBefore + 1).toString());
+  cy.get('h2.title').should((title) => {
+    expect(title.get()[0].innerText, titleBefore);
+  });
+  cy.get('article:first-child .rating span').should((rating) => {
+    expect(rating.get()[0].innerText, `${likesAfter}`);
+  });
   cy.get('article:first-child .rating').click();
 });
