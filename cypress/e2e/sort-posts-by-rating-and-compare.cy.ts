c4012078ac9e030591f2a6fe3d2002eda4b83062
complex-tube <stolya25@gmail.com>
2023-03-27 19:26:43 +0300

    Add sort by rating and check it test

---
+++ cypress/e2e/sort-posts-by-rating-and-compare.cy.ts
@@ -0,0 +1,23 @@
+it('Sort posts by rating and compare', () => {
+  cy.visit('/login');
+  cy.url().should('include', '/login');
+  cy.get('.form.nickname input').clear().type('admin');
+  cy.get('.form.password input').clear().type('admincool');
+  cy.get('.content .enter').click();
+  cy.url().should('not.include', 'login');
+  cy.url().should('include', '/');
+  cy.get('poly-sort-bar .bar .sort .item:nth-child(3)').should('exist');
+  cy.get('poly-sort-bar .bar .sort .item:nth-child(3)').click();
+  cy.get('poly-sort-bar .bar .options.active .items .chip:nth-child(4)').should('exist');
+  cy.get('poly-sort-bar .bar .options.active .items .chip:nth-child(4)').click();
+  cy.get('poly-sort-bar .bar .options.active button').should('exist');
+  cy.get('poly-sort-bar .bar .options.active button').click();
+  cy.get('poly-card:nth-of-type(1) article .footer .info .rating span').should('exist');
+  cy.get('poly-card:nth-of-type(2) article .footer .info .rating span').should('exist');
+  let secondArticleRating = 0;
+  cy.get('poly-card:nth-of-type(2) article .footer .info .rating span').then($rating => {
+    secondArticleRating = Number($rating.get()[0].innerText);
+  });
+  cy.get('poly-card:nth-of-type(1) article .footer .info .rating span').invoke('text').then(parseInt).should('be.greaterThan',
+    secondArticleRating)
+});
