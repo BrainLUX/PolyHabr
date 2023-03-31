2592cbc2d9a17941042fd2d5578093eebcd4b3b3
AD <Veselblu@yandex.ru>
2023-03-31 19:17:21 +0300

    Add add and edit article tests

--- cypress/e2e/fav-article.cy.ts
+++ cypress/e2e/fav-article.cy.ts
@@ -17,5 +17,15 @@ it('Fav article', () => {
     expect(title.get()[0].innerText, titleBefore);
   });
   cy.get('article:first-child .favourite svg').should("have.class", "selected");
-  cy.get('article:first-child .favourite').click();
+  cy.get('header .bottom .container .actions .button-icon:nth-child(3)').should((profileButton) => {
+    expect(profileButton, 'Profile button existing').to.exist;
+  });
+  cy.get('header .bottom .container .actions .button-icon:nth-child(3)').click();
+  cy.get('header .bottom .container .actions .button-icon:nth-child(3) poly-menu .menu .body .item:nth-child(2)').should("exist");
+  cy.get('header .bottom .container .actions .button-icon:nth-child(3) poly-menu .menu .body .item:nth-child(2)').click();
+  cy.get('poly-card:nth-child(2) article .title').should((title) => {
+    expect(title.get()[0].innerText, titleBefore);
+  });
+  cy.get('poly-card:nth-child(2) article .favourite svg').should("have.class", "selected");
+  cy.get('poly-card:nth-child(2) article .favourite').click();
 });
