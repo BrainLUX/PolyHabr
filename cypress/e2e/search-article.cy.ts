d100ecf603fb23fc6a7989f2dc3f396d419fff12
complex-tube <stolya25@gmail.com>
2023-04-01 02:06:48 +0300

    Fix search article ui test

--- cypress/e2e/search-article.cy.ts
+++ cypress/e2e/search-article.cy.ts
@@ -1,12 +1,12 @@
 it('Search article', () => {
   cy.visit('/');
   cy.get('poly-header .actions a:first-child').click();
-  const searchTitle = 'Test';
+  const searchTitle = 'TestEdited';
   cy.url().should('include', '/search');
   cy.get('.bar .search input').clear().type(searchTitle);
   cy.get('.bar .search svg').click();
   cy.get('.container').should('have.length.above', 1);
-  cy.get('article > a').click();
+  cy.get('.container poly-card:nth-of-type(1) article > a').click();
   cy.url().should('include', '/article');
   cy.url().should('not.include', '/search');
   cy.get('h2.title').should('have.text', searchTitle);
